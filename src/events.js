30c98b777d7da4518797f9cdef41c5490d4b170a
Pau Fracés <pfraces@gmail.com>
2015-09-10 03:22:38 +0200

    methods are not chained anymore
    they are expected to be mixed into a chainable set of functions

--- src/events.js
+++ src/events.js
@@ -1,11 +1,11 @@
-define('mu.async.events', function (require) {
+define('mu.events', function (require) {
   'use strict';
 
   var isDefined = require('mu.is.defined'),
       apply     = require('mu.fn.apply'),
+      partial   = require('mu.fn.partial'),
       defer     = require('mu.fn.defer'),
-      each      = require('mu.list.each'),
-      chain     = require('mu.api.chain');
+      each      = require('mu.list.each');
 
   var on = function (listeners, event, listener) {
     if (!isDefined(listeners[event])) { listeners[event] = []; }
@@ -27,10 +27,10 @@ define('mu.async.events', function (require) {
   var events = function () {
     var listeners = {};
 
-    return chain({
-      on: on,
-      emit: emit
-    }, listeners);
+    return {
+      on: partial(on, listeners),
+      emit: partial(emit, listeners)
+    };
   };
 
   return events;
